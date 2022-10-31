bb27656fe9025351cde763750acabc8c040f7a8e
Daniel Haycraft <dhcopy1@gmail.com>
2022-10-31 15:41:20 -0700

    b

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-app.get("/styles", (req, res) {
-    res.sendFile(path.join__dirname, "../public/index.css")
+app.get("/styles", function (req, res) {
+    res.sendFile(path.join__dirname, "../public/index.css");
 });
 
 const port = process.env.PORT || 4005;
