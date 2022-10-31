89d9375814ec191e9b515a3bac9350cd7e0aee88
Daniel Haycraft <dhcopy1@gmail.com>
2022-10-31 16:01:21 -0700

    b

--- server/server.js
+++ server/server.js
@@ -7,16 +7,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public"));
-});
+//middleware
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public/index.css")));
+//END POINTS
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-app.get("/styles", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.css"));
-});
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "../public"));
+// });
+
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
+// app.get("/styles", function (req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.css"));
+// });
 
 const port = process.env.PORT || 4005;
 
